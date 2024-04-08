9e7ff1ab55095ec4cfa40c3c00f59346608c9da6
wukong01234 <khanha3cvp@gmail.com>
2024-04-08 22:00:50 +0700

    updatePL

--- Player.cpp
+++ Player.cpp
@@ -25,8 +25,8 @@ void Player::move(){
 	} else if(right){
 		dx = speed;
 	}
-	x = (x + dx < 0 ? 0 : (x + dx > 1000 - 80 ? 1000 - 80: x + dx));
-	y = (y + dy < 0 ? 0 : (y + dy > 650 - 45 ? 650 - 45 : y + dy));
+	x = (x + dx < 0 ? 0 : (x + dx > 1000 - this->W ? 1000 - this->W : x + dx));
+	y = (y + dy < 0 ? 0 : (y + dy > 650 - this->H ? 650 - this->H : y + dy));
 	dx = dy = 0;
 }
 bool Player :: is_move(){
@@ -36,23 +36,23 @@ bool Player :: is_move(){
 void Player::keyDown(SDL_KeyboardEvent *event){
 	if(!(event->repeat)){
 		switch(event->keysym.scancode){
-		case SDL_SCANCODE_W:
-		case SDL_SCANCODE_UP:
-			up = true;
-			break;
-		case SDL_SCANCODE_S:
-		case SDL_SCANCODE_DOWN:
-			down = true;
-			break;
-		case SDL_SCANCODE_A:
-		case SDL_SCANCODE_LEFT:
-			left = true;
-			break;
-		case SDL_SCANCODE_D:
-		case SDL_SCANCODE_RIGHT:
-			right = true;
-			break;
-	}
+            case SDL_SCANCODE_W:
+            case SDL_SCANCODE_UP:
+                up = true;
+                break;
+            case SDL_SCANCODE_S:
+            case SDL_SCANCODE_DOWN:
+                down = true;
+                break;
+            case SDL_SCANCODE_A:
+            case SDL_SCANCODE_LEFT:
+                left = true;
+                break;
+            case SDL_SCANCODE_D:
+            case SDL_SCANCODE_RIGHT:
+                right = true;
+                break;
+        }
     }
 }
 void Player::keyUp(SDL_KeyboardEvent *event){
@@ -81,29 +81,30 @@ void Player::mouseDown( int Newx , int Newy , SDL_Renderer* renderer){
 	Bullet* NewBullet = new Bullet() ;
 	NewBullet->setTexture( "Bullet.png" , renderer ) ;
     NewBullet->setHP(1) ;
+    int radius = sqrt((this->W) * ( this->W ) + (this->H) * ( this->H )) + 1 ;
     std :: cout << NewBullet->getW() <<' ' << NewBullet->getH() << "\n"  ;
-	double Deltax = (double)(Newx) - ( this->x + 80 / 2) ;
-	double Deltay = (double)(Newy) - ( this->y + 45 / 2) ;
-	if ( sqrt(Deltax * Deltay + Deltay * Deltay ) < 46 ){
+	double Deltax = (double)(Newx) - ( this->x + this->W / 2) ;
+	double Deltay = (double)(Newy) - ( this->y + this->H / 2) ;
+	if ( sqrt(Deltax * Deltay + Deltay * Deltay ) < radius ){
         NewBullet->release() ; return ;
 	}
     if ( Deltax == 0 ){
         NewBullet->setX(this->x) ;
-        NewBullet->setY(this->y + 46 * ( Newy > this->y ? 1 : -1 )) ;
+        NewBullet->setY(this->y + radius * ( Newy > this->y ? 1 : -1 )) ;
     } else
     if ( Deltay == 0 ){
         NewBullet->setY(this->y) ;
-        NewBullet->setX(this->x + 46 * ( Newx > this->x ? 1 : -1 )) ;
+        NewBullet->setX(this->x + radius * ( Newx > this->x ? 1 : -1 )) ;
     }
     else{
         double Ratio = Deltax / Deltay ;
         Ratio = Ratio * Ratio ;
         double _DX = speed * speed /( 1 + Ratio ) * Ratio ;
         double _DY = speed * speed - _DX ;
-        double _GX = (double)(46 * 46) /( 1 + Ratio ) * Ratio ;
-        double _GY = (double)(46 * 46) - _GX ;
-        double _LX = ( this->x + 80 / 2) + (sqrt(_GX) )* ( Deltax > 0 ? 1 : -1 );
-        double _LY = ( this->y + 45 / 2) + (sqrt(_GY) )* ( Deltay > 0 ? 1 : -1 ) ;
+        double _GX = (double)(radius * radius) /( 1 + Ratio ) * Ratio ;
+        double _GY = (double)(radius * radius) - _GX ;
+        double _LX = ( this->x + this->W / 2) + (sqrt(_GX) )* ( Deltax > 0 ? 1 : -1 );
+        double _LY = ( this->y + this->H / 2) + (sqrt(_GY) )* ( Deltay > 0 ? 1 : -1 ) ;
         NewBullet->setX(_LX - 10 ) ;
         NewBullet->setY(_LY - 10 ) ;
     }
